4fff4fbfcee61aacde253d37f6fb6b4266d875df
Staval <stalkervaultboy@gmail.com>
2023-07-05 01:17:57 +0400

    Initial commit

---
+++ src/main/java/com/example/summerpractice/model/Category.java
@@ -0,0 +1,15 @@
+package com.example.summerpractice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    private long id;
+    private String name;
+}
