bf16ff9a1d4f7d506b2a7754517761a2d6eca28d
Staval <stalkervaultboy@gmail.com>
2023-07-09 23:10:45 +0400

    Some changes and added test controller for home page

--- src/main/java/com/example/summerpractice/controller/CategoryController.java
+++ src/main/java/com/example/summerpractice/controller/CategoryController.java
@@ -8,11 +8,7 @@ import com.example.summerpractice.service.CategoryService;
 import com.example.summerpractice.util.ResponseFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
